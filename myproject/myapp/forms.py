5d81650600014926a37facba17558fc65d1ece7f
elive7 <parksub020704@gmail.com>
2022-07-02 08:39:32 +0900

    수정

--- myproject/myapp/forms.py
+++
@@ -1,9 +0,0 @@
-from .models import Comment
-from django import forms
-from .models import Comment
-
-
-class CommentForm(forms.ModelForm): #django model form
-    class Meta:
-        model=Comment
-        fields=['comment']
